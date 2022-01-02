16d2d2fdb53357391b55b174f032b04f09175fe3
Laurent David <lmedavid@gmail.com>
2022-01-02 20:12:49 +0100

    Display activity navigation as a header

--- lib.php
+++ lib.php
@@ -215,6 +215,88 @@ class format_envfpsup extends format_base {
     }
 
     /**
+     * This is a copy of the activity navigation but displayed as a header
+     *
+     * See {@link format_base::course_header()} for usage
+     *
+     * @return null|renderable null for no output or object with data for plugin renderer
+     */
+    public function course_content_header() {
+        global $PAGE;
+        // First we should check if we want to add navigation.
+        $context = $PAGE->context;
+        if (($PAGE->pagelayout !== 'incourse' && $PAGE->pagelayout !== 'frametop')
+            || $context->contextlevel != CONTEXT_MODULE) {
+            return null;
+        }
+
+        // If the activity is in stealth mode, show no links.
+        if ($PAGE->cm->is_stealth()) {
+            return null;
+        }
+
+        // Get a list of all the activities in the course.
+        $course = $PAGE->cm->get_course();
+        $modules = get_fast_modinfo($course->id)->get_cms();
+
+        // Put the modules into an array in order by the position they are shown in the course.
+        $mods = [];
+        $activitylist = [];
+        foreach ($modules as $module) {
+            // Only add activities the user can access, aren't in stealth mode and have a url (eg. mod_label does not).
+            if (!$module->uservisible || $module->is_stealth() || empty($module->url)) {
+                continue;
+            }
+            $mods[$module->id] = $module;
+
+            // No need to add the current module to the list for the activity dropdown menu.
+            if ($module->id == $PAGE->cm->id) {
+                continue;
+            }
+            // Module name.
+            $modname = $module->get_formatted_name();
+            // Display the hidden text if necessary.
+            if (!$module->visible) {
+                $modname .= ' ' . get_string('hiddenwithbrackets');
+            }
+            // Module URL.
+            $linkurl = new moodle_url($module->url, array('forceview' => 1));
+            // Add module URL (as key) and name (as value) to the activity list array.
+            $activitylist[$linkurl->out(false)] = $modname;
+        }
+
+        $nummods = count($mods);
+
+        // If there is only one mod then do nothing.
+        if ($nummods == 1) {
+            return null;
+        }
+
+        // Get an array of just the course module ids used to get the cmid value based on their position in the course.
+        $modids = array_keys($mods);
+
+        // Get the position in the array of the course module we are viewing.
+        $position = array_search($PAGE->cm->id, $modids);
+
+        $prevmod = null;
+        $nextmod = null;
+
+        // Check if we have a previous mod to show.
+        if ($position > 0) {
+            $prevmod = $mods[$modids[$position - 1]];
+        }
+
+        // Check if we have a next mod to show.
+        if ($position < ($nummods - 1)) {
+            $nextmod = $mods[$modids[$position + 1]];
+        }
+
+        $activitynav = new \core_course\output\activity_navigation($prevmod, $nextmod, $activitylist);
+        return $activitynav;
+    }
+
+
+    /**
      * Definitions of the additional options that this course format uses for course
      *
      * Course display must be defined
